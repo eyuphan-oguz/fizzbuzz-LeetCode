c1d709ae6f211f2678674617d8d95c2444274781
Eyuphan Oguz <75530935+eyuphan-oguz@users.noreply.github.com>
2023-10-18 10:06:48 +0300

    first commit

---
+++ Solution.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public static List<String> fizzBuzz(int n) {
+        List<String> answer = new ArrayList<String>();
+        for(int i = 1 ; i<= n ; i++){
+            if(i % 3 ==0 && i % 5 == 0){
+                answer.add("FizzBuzz");
+            }else if(i % 3 == 0){
+                 answer.add("Fizz");
+            }else if (i % 5 == 0){
+                answer.add("Buzz");
+            }else{
+                answer.add(i+ "");
+            }
+        }
+        System.out.println(answer);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        
+        fizzBuzz(5);
+       
+    }
+}
+
+
+
+
+
+
+
